4e86dbb05e80a69d9a0dd827e0a980fd5380e334
stvnmc <stvnmc123@gmail.com>
2024-07-10 21:09:38 -0600

    finish crud

--- src/app/api/student/[id]/route.js
+++ src/app/api/student/[id]/route.js
@@ -12,11 +12,35 @@ export const GET = async (request, { params }) => {
     return NextResponse.json({ data: null }, { status: 500 });
   }
 };
-export const DELETE = async (request, { params }) => {
+
+export const PUT = async (request, { params }) => {
   await connectDB();
   const id = params.id;
   try {
     const result = await StudentModel.findByIdAndDelete(id);
+    if (!result) {
+      return NextResponse.json(
+        { message: `Doncument with ID: ${id} not found` },
+        { status: 200 }
+      );
+    }
+    return NextResponse.json({ data: result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ data: null }, { status: 500 });
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  await connectDB();
+  const id = params.id;
+
+  const body = await request.json();
+  try {
+    const result = await StudentModel.findByIdAndUpdate(
+      id,
+      { $set: { ...body } },
+      { new: true }
+    );
     if (!result) {
       return NextResponse.json(
         { message: `Doncument with ID: ${id} not found` },
